7510e4b8623a3045bbea13312f8ccebc9649d3a5
arnav-sys <74288659+arnav-sys@users.noreply.github.com>
2022-01-08 18:22:50 +0530

    Add files via upload

---
+++ tests_model.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  8 18:05:15 2022
+
+@author: akhil
+
+"""
+
+import joblib
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("final_data.csv")
+df.drop(columns=["Unnamed: 0"],inplace=True)
+
+#splitting dataset into train, test and validate
+X = df.drop(columns=["AverageTemperature"])
+y = df["AverageTemperature"] 
+
+
+
+
+def test_model():
+     model = joblib.load("model.pkl")
+    
+     elements_pred =  model.predict(X)
+     
+     from sklearn.metrics import mean_squared_error
+     error = np.sqrt(mean_squared_error(y,elements_pred))
+     print(error)
+     assert error < 0.9
+     
+test_model()
